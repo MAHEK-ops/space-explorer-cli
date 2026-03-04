b93f9fd856733ff997e86dec18ded620a3df40fd
Mahek Motwani <muskanmotwani0611@gmail.com>
2026-03-04 23:15:52 +0530

    Add APOD command to fetch astronomy picture of the day

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { Command } from "commander"
 import figlet from "figlet"
+import { NasaService } from "./services/nasaService"
+
 
 const program = new Command()
 console.log(figlet.textSync("SPACE CLI"))
+const nasaService = new NasaService()
 
+program
+ .command("apod")
+ .description("Astronomy Picture of the Day")
+ .action(async () => {
+
+   const data = await nasaService.getAPOD()
+
+   console.log("Title:", data.title)
+   console.log("Explanation:", data.explanation)
+   console.log("Image:", data.url)
+
+ })
 program
     .name("space")
     .description("Space Explorer CLI")
--- src/services/nasaService.ts
+++ src/services/nasaService.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 
+
 export class NasaService {
 
  async getAPOD() {
