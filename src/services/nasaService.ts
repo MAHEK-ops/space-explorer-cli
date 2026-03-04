f52177740e3190c3f7d5be58e6bf7bc62471d019
Mahek Motwani <muskanmotwani0611@gmail.com>
2026-03-04 23:15:02 +0530

    Add NASA service class to fetch astronomy data

---
+++ src/services/nasaService.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+export class NasaService {
+
+ async getAPOD() {
+
+   const response = await axios.get(
+     "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+   )
+
+   return response.data
+ }
+
+}
