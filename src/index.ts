fdb8abd7a35d5101ec2008c87cebd1a057c07735
Mahek Motwani <muskanmotwani0611@gmail.com>
2026-03-04 23:50:18 +0530

    Finalize Space Explorer CLI: added 10 commands, integrated NASA, ISS and Space News APIs, and cleaned command structure

--- src/index.ts
+++ src/index.ts
@@ -12,32 +12,37 @@ const issService = new ISSService()
 const newsService = new NewsService()
 
 program
-  .name("space")
-  .description("Space Explorer CLI")
-  .version("1.0.0")
+    .name("space")
+    .description("Space Explorer CLI")
+    .version("1.0.0")
 
 // APOD
 program.command("apod")
 .description("Astronomy Picture of the Day")
 .action(async () => {
 
-  const data = await nasaService.getAPOD()
+    const data = await nasaService.getAPOD()
 
-  console.log("Title:", data.title)
-  console.log("Explanation:", data.explanation)
-  console.log("Image:", data.url)
+    console.log("Title:", data.title)
+    console.log("Explanation:", data.explanation)
+    console.log("Image:", data.url)
 
 })
 
-// Mars photos
-program.command("mars")
-.description("Mars Rover Photos")
+// showing asteroid data instead
+program.command("asteroids-today")
+.description("Space objects near Earth")
 .action(async () => {
 
-  const data = await nasaService.getMarsPhotos()
+    const data = await nasaService.getTodayAsteroids()
 
-  console.log("Mars Rover Photo:")
-  console.log(data.photos[0].img_src)
+    const today = Object.keys(data.near_earth_objects)[0]
+
+    console.log("Near Earth Asteroids:")
+
+    data.near_earth_objects[today].slice(0,3).forEach((a:any)=>{
+        console.log(a.name)
+    })
 
 })
 
@@ -46,9 +51,12 @@ program.command("asteroids")
 .description("Near Earth Asteroids")
 .action(async () => {
 
-  const data = await nasaService.getAsteroids()
+    const data = await nasaService.getAsteroids()
 
-  console.log("Asteroid Name:", data.near_earth_objects[Object.keys(data.near_earth_objects)[0]][0].name)
+    const today = Object.keys(data.near_earth_objects)[0]
+
+    console.log("Asteroid Name:",
+        data.near_earth_objects[today][0].name)
 
 })
 
@@ -57,10 +65,10 @@ program.command("iss")
 .description("Current ISS Location")
 .action(async () => {
 
-  const data = await issService.getISSLocation()
+    const data = await issService.getISSLocation()
 
-  console.log("Latitude:", data.iss_position.latitude)
-  console.log("Longitude:", data.iss_position.longitude)
+    console.log("Latitude:", data.iss_position.latitude)
+    console.log("Longitude:", data.iss_position.longitude)
 
 })
 
@@ -69,13 +77,13 @@ program.command("astronauts")
 .description("Astronauts currently in space")
 .action(async () => {
 
-  const data = await issService.getAstronauts()
+    const data = await issService.getAstronauts()
 
-  console.log("People in Space:", data.number)
+    console.log("People in Space:", data.number)
 
-  data.people.forEach((person: any) => {
-    console.log(person.name)
-  })
+    data.people.forEach((person:any)=>{
+        console.log(person.name)
+    })
 
 })
 
@@ -84,10 +92,10 @@ program.command("news")
 .description("Latest space news")
 .action(async () => {
 
-  const news = await newsService.getNews()
+    const news = await newsService.getNews()
 
-  console.log(news.results[0].title)
-  console.log(news.results[0].url)
+    console.log(news.results[0].title)
+    console.log(news.results[0].url)
 
 })
 
@@ -96,20 +104,20 @@ program.command("planet")
 .description("Random planet fact")
 .action(() => {
 
-  const planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+    const planets = ["Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune"]
 
-  const random = planets[Math.floor(Math.random() * planets.length)]
+    const random = planets[Math.floor(Math.random()*planets.length)]
 
-  console.log("Random Planet:", random)
+    console.log("Random Planet:", random)
 
 })
 
-// Galaxy command
+// Galaxy
 program.command("galaxy")
 .description("About Milky Way")
 .action(() => {
 
-  console.log("Our galaxy is the Milky Way 🌌")
+    console.log("Our galaxy is the Milky Way")
 
 })
 
@@ -118,16 +126,16 @@ program.command("launches")
 .description("Upcoming rocket launches")
 .action(() => {
 
-  console.log("Check NASA website for upcoming launches 🚀")
+    console.log("Check NASA website for upcoming launches")
 
 })
 
-// Help info
+// Info
 program.command("info")
 .description("About Space CLI")
 .action(() => {
 
-  console.log("Space Explorer CLI helps explore space data from terminal")
+    console.log("Space Explorer CLI helps explore space data from terminal")
 
 })
 
--- src/services/nasaService.ts
+++ src/services/nasaService.ts
@@ -2,31 +2,31 @@ import axios from "axios"
 
 export class NasaService {
 
- async getAPOD() {
+    async getAPOD() {
 
-  const response = await axios.get(
-   "https://api.nasa.gov/planetary/apod?api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
-  )
+        const response = await axios.get(
+            "https://api.nasa.gov/planetary/apod?api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
+        )
 
-  return response.data
- }
+        return response.data
+    }
 
- async getMarsPhotos() {
+    async getTodayAsteroids() {
 
-  const response = await axios.get(
-   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
-  )
+        const response = await axios.get(
+            "https://api.nasa.gov/neo/rest/v1/feed?api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
+        )
 
-  return response.data
- }
+        return response.data
+    }
 
- async getAsteroids() {
+    async getAsteroids() {
 
-  const response = await axios.get(
-   "https://api.nasa.gov/neo/rest/v1/feed?api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
-  )
+        const response = await axios.get(
+            "https://api.nasa.gov/neo/rest/v1/feed?api_key=hQ1wRW31qXatEDdDYAeLugj9DuniiBFIZq3qdbkG"
+        )
 
-  return response.data
- }
+        return response.data
+    }
 
 }
